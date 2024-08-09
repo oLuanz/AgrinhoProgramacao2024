3856c07babddcadf2861643de10a70e6b602a85e
oLuanz <145064542+oLuanz@users.noreply.github.com>
2024-08-09 14:28:22 -0300

    jdjks

---
+++ js/logic.js
@@ -0,0 +1,22 @@
+  var prevScrollpos = window.scrollY;
+
+/* Get the header element and it's position */
+var headerDiv = document.querySelector("header");
+var headerBottom = headerDiv.offsetTop + headerDiv.offsetHeight;
+
+window.onscroll = function() {
+  var currentScrollPos = window.scrollY;
+
+  /* if we're scrolling up, or we haven't passed the header,
+     show the header at the top */
+  if (prevScrollpos > currentScrollPos  || currentScrollPos < headerBottom){  
+      headerDiv.style.top = "0";
+  }
+  else{
+      /* otherwise we're scrolling down & have passed the header so hide it */
+      headerDiv.style.top = "-7.2rem";
+  } 
+
+  prevScrollpos = currentScrollPos;
+}
+
